dd0ab312aea8b7879947d578237d8a97035501b9
Philip Jurke <pjurke@icloud.com>
2024-07-19 18:03:45 +0200

    add basic footer

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer>
+            <Link href="/">Element A</Link>
+            <Link href="/">Element B</Link>
+            <Link href="/">Element C</Link>
+        </footer>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopMenu from "./components/top-level-menu/TopMenu";
+import Footer from "./components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <TopMenu />
         {children}
+        <Footer />
       </body>
     </html>
   );
