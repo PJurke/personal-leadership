078bdaaa6bebf33f56ba7b974aba3eb3b6f76175
Philip Jurke <pjurke@icloud.com>
2024-07-19 15:44:05 +0200

    Add examplary menu items

--- app/components/top-level-menu/TopMenu.tsx
+++ app/components/top-level-menu/TopMenu.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function TopMenu() {
     return (
-        <nav className="bg-gray-500 fixed p-4 top-0 w-full">
-            Top Menu
+        <nav className="bg-orange-300 fixed p-4 top-0 w-full">
+            <ul className="flex flex-col items-center md:flex-row md:space-x-4">
+                <li><Link href="/">Punkt A</Link></li>
+                <li><Link href="/">Punkt B</Link></li>
+                <li><Link href="/">Punkt C</Link></li>
+                <li><Link href="/">Punkt D</Link></li>
+                <li><Link href="/">Punkt E</Link></li>
+            </ul>
         </nav>
     );
 }
