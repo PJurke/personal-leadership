407156e72ff6997db7fb4df75601d64109772247
Philip Jurke <pjurke@icloud.com>
2024-07-19 18:10:25 +0200

    add dynamic footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 
 export default function Footer() {
+
+    const links = [
+        { label: "Element A", href: "/"},
+        { label: "Element B", href: "/"},
+        { label: "Element C", href: "/"},
+    ];
+
     return (
-        <footer>
-            <Link href="/">Element A</Link>
-            <Link href="/">Element B</Link>
-            <Link href="/">Element C</Link>
+        <footer className="bg-gray-700 flex justify-center p-4 space-x-16 text-white">
+            {links.map((item, index) => 
+                <Link key={index} href={item.href}>{item.label}</Link>
+            )}
         </footer>
     );
 }
